125d40fdf376e210f868c2f87abf64622f51deaf
JuanManuel B <BETA1JUA@police.nsw.gov.au>
2017-07-04 17:18:38 +1000

    14 added

--- 05_destructuring/14-destructuring-parameters.js
+++ 05_destructuring/14-destructuring-parameters.js
@@ -1,20 +1,25 @@
 // 14: destructuring - parameters
 // To do: make all tests pass, leave the assert lines unchanged!
 
+var assert = require("assert");
+
 describe('destructuring function parameters', () => {
 
   describe('destruct parameters', () => {
+
+//revisa, no lo vi en primera pasada
     it('multiple params from object', () => {
-      const fn = ({id}, {name}) => {
+      const fn = ({id, name}) => {
         assert.equal(id, 42);
         assert.equal(name, 'Wolfram');
       };
       const user = {name: 'Wolfram', id: 42};
       fn(user);
     });
-    
+
+//wow mira aprende las comas para arraya
     it('multiple params from array/object', () => {
-      const fn = ([{name}]) => {
+      const fn = ([,{name}]) => {
         assert.equal(name, 'Alice');
       };
       const users = [{name: 'nobody'}, {name: 'Alice', id: 42}];
@@ -24,23 +29,32 @@ describe('destructuring function parameters', () => {
 
   describe('default values', () => {
     it('for simple values', () => {
-      const fn = (id, name='Bobby') => {
+      const fn = (id, name='Bob') => {
         assert.strictEqual(id, 23);
         assert.strictEqual(name, 'Bob');
       };
       fn(23);
     });
-    
+
+    // it('for a missing array value', () => {
+    //   const defaultUser = {id: 23, name: 'Joe'};
+    //   const fn = ([user]) => {
+    //     assert.deepEqual(user, defaultUser);
+    //   };
+    //   fn([]);
+    // });
+
+//esta perris! ni piu
     it('for a missing array value', () => {
       const defaultUser = {id: 23, name: 'Joe'};
-      const fn = ([user]) => {
+      const fn = ([user = {id: 23, name: 'Joe'}] ) => {
         assert.deepEqual(user, defaultUser);
       };
       fn([]);
     });
-    
+
     it('mix of parameter types', () => {
-      const fn = (id, [arr], {obj}) => {
+      const fn = (id =1 , [arr = 2], {obj = 3}) => {
         assert.equal(id, 1);
         assert.equal(arr, 2);
         assert.equal(obj, 3);
